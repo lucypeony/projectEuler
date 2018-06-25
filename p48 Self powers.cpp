ed001f432aa419dd80f52e50ab16f4f88d38aeff
lucypeony <lucypeony@hotmail.com>
2018-06-25 16:18:47 +0800

    Modular exponentiation

--- p48 Self powers.cpp
+++ p48 Self powers.cpp
@@ -6,23 +6,21 @@ Euler Project p48 Self powers
 #include<iostream>
 using namespace std; 
 
-const int M = 1000000000000;
-//return the last digit 
+const long long  M = 10000000000;
+//return the last 10 digits 
 long long last10(int x,int y) {
-	if (y == 1)
-		return x % M;
+	long long res = 1;
+	for (int i = 1; i <= y; i++) {
+		res *= x;
+		res %= M;
+	}
 
-	int y1 = y / 2;
-	int y2 = y - y1;
-	long long  r1 = last10(x, y1);
-	long long  r2 = last10(x, y2);
-	return (r1*r2) % M;
-	//return (last10(x, y1)*last10(x, y2)) % M;
+	return res;
 }
 
 void solve() {
 	long long res = 0;
-	for (int i = 1; i <= 12; i++) {
+	for (int i = 1; i <= 1000; i++) {
 		long long temp=last10(i,i);
 		cout << i << " " << temp << endl;
 		res += temp;
@@ -33,7 +31,5 @@ void solve() {
 
 int main() {
 	solve();
-
-	cout << "Hello World！" << endl;
 	return 0;
 }
