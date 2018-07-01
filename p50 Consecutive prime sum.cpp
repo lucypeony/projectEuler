98e774187bbcf0a2db6230c0acc7c5bb0cf23c89
lucypeony <lucypeony@hotmail.com>
2018-07-01 11:35:16 +0800

    brute force

--- p50 Consecutive prime sum.cpp
+++ p50 Consecutive prime sum.cpp
@@ -2,6 +2,12 @@
 lucypeony@hotmail.com
 
 Euler Project p50 Consecutive prime sum
+----------------------------------------
+这一题其实很简单，因为是求100万以下的素数是哪些连续素数的和，所以我就求出了100万以下的素数，并打算求哪些个素数是哪些连续素数的和。
+可想而知，这个方法非常慢。
+其实，因为要求的是那个素数必定是某些连续素数的和，而且素数又是递增的。所以，我可以先求出从2,3,4,... 的和，看多少能达到100万。
+实际上，这些素数个数很少！只有547个！也就是p1,p2,....,p547 的和就已经超过100万了。
+因此我们只要对547个素数进行分别求连续和，看最长连续是多少就行了。
 */
 #include<vector>
 #include<algorithm>
@@ -30,32 +36,44 @@ vector<int> generate_primes() {
 	return primes;
 }
 
+bool isPrime(long long n) {
+	for (int i = 2; i*i < n; i += 1)
+		if (n%i == 0)
+			return false;
+
+	return true;
+}
+
 void solve() {
+	ofstream fout("p50.out");
 	vector<int> primes = generate_primes();
-	//const int NUM = 87498; //num of primes under 1 million 
-	long long sum = 0;
-	long long max_con = -1;
-	for (int i = 0; i < primes.size(); i++) {
-		for (int j = i + 1; j < primes.size(); j+=2) {
-			//calculate if sum[i..j] is prime,if so, store j-i and the sum;
-			long long temp = 0;
-			int k = i;
-			while (k <= j) {
-				temp += primes[k];
-				k++;
-			}
-			if (find(primes.begin(), primes.end(), temp) != primes.end()) { //if sum is prime
-				{
-					if (j - i + 1 > max_con) {
-						max_con = j - i + 1;
-						sum = temp;
-					}
-				}
+	vector<int> sum;
+
+	int count = 0;
+	long long temp = 0;
+	while (temp <= 1000000) {
+		temp += primes[count];
+		sum.push_back(temp);
+		count++;
+	}
+	fout << temp << endl;
+	fout << count << endl;
+
+	int margin = 0;
+	int max_primes = -1;
+	for (int i = 0; i < count; i++) {
+		for (int j = i + 1; j < count; j++)
+		{
+			temp = sum[j] - sum[i];
+			if (isPrime(temp) && j - i + 1 > margin) {
+				max_primes = temp;
+				margin = j - i + 1;
 			}
 		}
 	}
-	ofstream fout("p50.out");
-	fout << max_con << "  " << sum << endl;
+
+	fout << "Margin : " << margin << endl;
+	fout << "Max Primes: " << max_primes << endl;
 	fout.close();
 }
 
