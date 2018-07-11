2080ad4ec89f743dd398ea3a9f2cd09f0ba9a81e
lucypeony <lucypeony@hotmail.com>
2018-07-11 20:59:43 +0800

    ad hoc ; brute force

--- p25 1000-digit Fibonacci number.cpp
+++ p25 1000-digit Fibonacci number.cpp
@@ -1,29 +1,20 @@
-/*
-lucypeony@hotmail.com
-p25 1000-digit Fibonacci number
-The 12th term, F12,144, is the first term to contain three digits.
-What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
---------------------------------------------------------------------------------------
-Big Integer Addition
-*/
 #include<iostream>
 #include<fstream>
 #include<string>
-
-using namespace std;
+using namespace std; 
 
 string add(string a, string b) {
 	if (a.size()<b.size())
 		swap(a, b);
 	int len_a = a.size();
 	int len_b = b.size();
-	
+
 
 	int i = len_a - 1;
 	int j = len_b - 1;
 	int c = 0;
 	while (i >= 0 && j >= 0) {
-		int temp = a[i] - '0' + b[j] - '0'+c;
+		int temp = a[i] - '0' + b[j] - '0' + c;
 		c = temp / 10;
 		temp %= 10;
 		a[i] = '0' + temp;
@@ -47,36 +38,31 @@ string add(string a, string b) {
 	return a;
 }
 
-void solve() {
-	ofstream fout("p25.out");
-	string f1 = "1";
-	string f2 = "1";
-	//fout << f1 << endl;
-	//fout << f2 << endl;
-	int count = 2;
-	while (1) {
-		f1 = add(f1, f2);
-		count++;
-		//fout << f1 << endl;
-		if (f1.size() == 1000)
-			break;
-		
-		f2 = add(f1, f2);
-		count++;
-		//fout << f2 << endl;
-		if (f2.size() == 1000)
-			break;
-		
-	}
-
-	string res = f1.size() > f2.size() ? f1 : f2;
-	fout << "res:" << res << endl;
-	fout << count << endl;
-	
-	fout.close();
-}
 
 int main() {
-	solve();
+	int n = 1000;
+	
+	ofstream fout("p57.out");
+	int ans = 0;
+	string numerator = "1";
+	string denominator = "2";
+	while (n > 1) {
+		//1+numerator/denominator
+		numerator = add(numerator, denominator);
+		
+		if (numerator.size() > denominator.size()) {
+			ans++;
+		}
+		//fout <<1001-n<<" "<< numerator << "/" << denominator << endl;
+		//1+numerator/denominator
+		numerator = add(numerator, denominator);
+		//求倒数
+		swap(numerator, denominator);
+		n--;
+	}
+	numerator = add(numerator, denominator);
+	fout << numerator << "/" << denominator << endl;
+	fout << ans;
+	fout.close();
 	return 0;
 }
