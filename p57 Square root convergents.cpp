d5bf7115bad4b2cb7463445a3abbe6e3c842bc28
lucypeony <lucypeony@hotmail.com>
2018-07-12 16:01:53 +0800

    brute force

--- p57 Square root convergents.cpp
+++ p57 Square root convergents.cpp
@@ -1,49 +1,68 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-#define ul unsigned long long 
 using namespace std; 
 
-ul gcd(ul a, ul b) {
-	ul r = a % b;
-	while (r > 0) {
-		a = b;
-		b = r;
-		r = a % b;
+string add(string a, string b) {
+	if (a.size()<b.size())
+		swap(a, b);
+	int len_a = a.size();
+	int len_b = b.size();
+
+
+	int i = len_a - 1;
+	int j = len_b - 1;
+	int c = 0;
+	while (i >= 0 && j >= 0) {
+		int temp = a[i] - '0' + b[j] - '0' + c;
+		c = temp / 10;
+		temp %= 10;
+		a[i] = '0' + temp;
+		i--;
+		j--;
 	}
-	return b;
+
+	while (i >= 0) {
+		int temp = a[i] - '0' + c;
+		c = temp / 10;
+		temp %= 10;
+		a[i] = '0' + temp;
+		i--;
+	}
+
+	if (c > 0) {
+		char t(c + '0');
+		a = t + a;
+	}
+
+	return a;
 }
+
 
 int main() {
 	int n = 1000;
 	
 	ofstream fout("p57.out");
 	int ans = 0;
-	ul numerator = 1;
-	ul denominator = 2;
+	string numerator = "1";
+	string denominator = "2";
 	while (n > 1) {
-		//2+numerator/denominator
-		numerator +=  denominator;
-		ul gcd1 = gcd(numerator, denominator);
-		if (gcd1 > 1) {
-			numerator /= gcd1;
-			denominator /= gcd1;
-			fout <<1001-n<< "  gcd1 " << gcd1 << endl;
-		}
-		if (to_string(numerator).size() > to_string(denominator).size()) {
-			//fout << numerator << "/" << denominator << endl;
+		//1+numerator/denominator
+		numerator = add(numerator, denominator);
+		
+		if (numerator.size() > denominator.size()) {
 			ans++;
 		}
-		fout <<1001-n<<" "<< numerator << "/" << denominator << endl;
-		numerator += denominator;
+		//fout <<1001-n<<" "<< numerator << "/" << denominator << endl;
+		//1+numerator/denominator
+		numerator = add(numerator, denominator);
 		//求倒数
 		swap(numerator, denominator);
 		n--;
 	}
-	numerator += denominator;
+	numerator = add(numerator, denominator);
 	fout << numerator << "/" << denominator << endl;
 	fout << ans;
-	cout << "Hello World!" << endl;
 	fout.close();
 	return 0;
 }
